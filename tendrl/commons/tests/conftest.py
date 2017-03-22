323c1f07b2a594371fbc20ea8876576aba72105e
Filip Balák <fbalak@redhat.com>
2017-03-22 07:13:34 -0400

    optimize regular expressions

--- tendrl/commons/tests/conftest.py
+++ tendrl/commons/tests/conftest.py
@@ -7,22 +7,26 @@ import re
 def definitions_node_agent_path_yaml():
     """Get absolute path to definitions file."""
 
-    return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..","..","..","etc/tendrl_definitions_node_agent.yaml"))
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "..","..","..","etc/tendrl_definitions_node_agent.yaml"))
 
 def get_run_attributes_from_yaml(file_path):
     """Get list of runable functions from definitions."""
 
     functions = set()
     list_search = False
+    pattern_run = re.compile("run:\s*(?P<run>[a-zA-Z0-9\._]+)")
+    pattern_prun = re.compile("- (?P<_run>[a-zA-Z0-9\._]+)")
     with open(file_path, 'r') as f:
         for line in f:
             if not list_search:
-                match = re.search("run:\s*(?P<run>[a-zA-Z0-9\._]+)", line)
+                match = pattern_run.search(line)
                 if match is not None:
                     functions.add(match.group("run"))
                     list_search = False
             else:
-                match = re.search("- (?P<_run>[a-zA-Z0-9\._]+)", line)
+                match = pattern_prun.search(line)
                 if match is not None:
                     functions.add(match.group("_run"))
                 else:
@@ -33,7 +37,8 @@ def get_run_attributes_from_yaml(file_path):
     return list(functions)
 
 
-@pytest.fixture(params=get_run_attributes_from_yaml(definitions_node_agent_path_yaml()))
+@pytest.fixture(
+    params=get_run_attributes_from_yaml(definitions_node_agent_path_yaml()))
 def definitions_run_attribute(request):
     """Generate function names for import."""
 
